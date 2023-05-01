97682d09cce0527374361d6ff9b59dafc6c9a276
Luis Soto <luisjdk3@gmail.com>
2023-05-01 18:18:42 -0500

    proyecto lista de contactos

---
+++ lista.js
@@ -0,0 +1,26 @@
+let contactos = ["camilo rojas", "luis soto", "andres castro"];
+
+function agregarContacto(nombreCompleto, listaContactos) {
+  listaContactos.push(nombreCompleto);
+}
+
+function borrarContacto(nombreCompleto, listaContactos) {
+  const indice = listaContactos.indexOf(nombreCompleto);
+  if (indice !== -1) {
+    listaContactos.splice(indice, 1);
+  } else {
+    console.log("El contacto no existe en la lista");
+  }
+}
+
+function imprimirContactos(listaContactos) {
+  console.log("Lista de contactos:");
+  listaContactos.forEach((contacto) => {
+    console.log(contacto);
+  });
+}
+
+agregarContacto("stiven gonzalez", contactos);
+imprimirContactos(contactos);
+borrarContacto("andres castro", contactos);
+imprimirContactos(contactos);
